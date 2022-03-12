254d25ee9cce0f2ff00ae7b7c986348a76d66ce5
upchunk <habi6799@gmail.com>
2022-03-12 17:53:58 +0700

    Added Mouse Hover Tooltips, Fixed User Icon

--- social/models.py
+++ social/models.py
@@ -10,13 +10,15 @@ class Account(models.Model):
         return self.handle
 
 class Post(models.Model):
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
     postID = models.AutoField(primary_key=True)
     date = models.DateTimeField('Date Posted', auto_now=True)
     description = models.TextField(blank=True, null=True)
     likes = models.IntegerField(default=0, null=True)
     comments = models.IntegerField(default=0, null=True)
     image = models.URLField(null=True, blank=True, max_length=200)
+    profileImage = models.URLField(null=True, blank=True, max_length=200)
     
     def __str__(self):
         return self.description
